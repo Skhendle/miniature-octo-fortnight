f79b103c8872cf2361edb53dc7d15c8b89f944dd
Sandile Khendle <skhendle@gmail.com>
2022-04-22 15:13:32 +0200

    setting up playlist page

--- lib/features/spotify/presentation/pages/spotify_playlist_page.dart
+++ lib/features/spotify/presentation/pages/spotify_playlist_page.dart
@@ -1,11 +1,65 @@
 import 'package:flutter/material.dart';
 
-//TODO: complete this page - you may choose to change it to a stateful widget if necessary
-class SpotifyPlaylist extends StatelessWidget {
-  const SpotifyPlaylist({Key? key}) : super(key: key);
+class SpotifyPlaylist extends StatefulWidget {
+  SpotifyPlaylist({Key? key}) : super(key: key);
 
   @override
+  State<SpotifyPlaylist> createState() => _SpotifyPlaylistState();
+}
+
+class _SpotifyPlaylistState extends State<SpotifyPlaylist> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: true,
+            pinned: true,
+            snap: false,
+            centerTitle: false,
+            title: Text('Kindacode.com'),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.shopping_cart),
+                onPressed: () {},
+              ),
+            ],
+            bottom: AppBar(
+              title: Container(
+                width: double.infinity,
+                height: 40,
+                color: Colors.white,
+                child: Center(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Search for something',
+                        prefixIcon: Icon(Icons.search),
+                        suffixIcon: Icon(Icons.camera_alt)),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // Other Sliver Widgets
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Container(
+                height: 400,
+                child: Center(
+                  child: Text(
+                    'This is an awesome shopping platform',
+                  ),
+                ),
+              ),
+              Container(
+                height: 1000,
+                color: Colors.pink,
+              ),
+            ]),
+          ),
+        ],
+      ),
+    );
   }
 }
