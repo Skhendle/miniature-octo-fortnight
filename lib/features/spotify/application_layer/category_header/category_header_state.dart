4be6bf8cffb46fb95a1a73159d18b5554550f9eb
Sandile Khendle <skhendle@gmail.com>
2022-04-22 12:41:23 +0200

    completed album header state

--- lib/features/spotify/application_layer/category_header/category_header_state.dart
+++ lib/features/spotify/application_layer/category_header/category_header_state.dart
@@ -2,9 +2,13 @@ part of 'category_header_bloc.dart';
 
 abstract class CategoryHeaderState extends Equatable {
   const CategoryHeaderState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 class CategoryHeaderInitial extends CategoryHeaderState {}
+
+class CategoryHeaderView extends CategoryHeaderState {}
+
+class CategoryHeaderError extends CategoryHeaderState {}
