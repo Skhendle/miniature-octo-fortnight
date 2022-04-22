c5d9f35101683bfab41fbb92c30433413ada46c7
Sandile Khendle <skhendle@gmail.com>
2022-04-22 11:01:52 +0200

    completed catergory header api

---
+++ lib/features/spotify/constants.dart
@@ -0,0 +1,5 @@
+const String serverUrl =
+    'https://palota-jobs-africa-spotify-fa.azurewebsites.net/api/';
+const Map<String, String> headers = {
+  'x-functions-key': '21WgX6piPoeo2kGavN8pdJUMes0HbjuGO1B5itLl0cPzC40IywU3Xg=='
+};
---
+++ lib/features/spotify/data_layer/catergory_header_api.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_spotify_africa_assessment/features/spotify/constants.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../domain_layer/catergory_header_model.dart';
+
+class CatergoryHeaderAPI {
+  Future<CategoryHeader> getPlaylists(String categoryId) async {
+    final request = await http
+        .get(Uri.parse('${serverUrl}browse/categories/$categoryId/playlists'),
+            headers: headers)
+        .timeout(const Duration(seconds: 30))
+        .catchError((e) {
+      throw (['Network Connection Problem']);
+    });
+
+    if (request.statusCode == 200) {
+      var response = jsonDecode(request.body);
+      return CategoryHeader(
+          name: response['id'].toString().toUpperCase(),
+          imageUrl: response['icons'][0]['url']);
+    } else {
+      throw ('Invalid Request');
+    }
+  }
+}
